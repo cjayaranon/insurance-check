06849111655e5e217ccf4575df5770dff24a449d
Cyril John <cjayaranon@gmail.com>
2021-09-25 10:16:41 +0800

    Security - added 404 and 403 custom pages, probably not visible because debug = True - removed Edit option on the page header menu

--- dicks/dicks/urls.py
+++ dicks/dicks/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from . import views
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import include, path
