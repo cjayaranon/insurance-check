4a38ca794963537250f38891502328feae37fc9b
cjayaranon <cyril2ampc@4b5d82362e5e>
2021-10-06 02:15:09 +0000

    changes from server 1st deployment

--- dicks/enroller/models.py
+++ dicks/enroller/models.py
@@ -19,7 +19,7 @@ class Role(models.Model):
 class Branch(models.Model):
     branch_name = models.CharField(max_length=32)
     address = models.CharField(max_length=120)
-    
+
     def __str__(self):
         return self.branch_name
 
@@ -32,7 +32,7 @@ class Client(models.Model):
         ('OTHER', 'Other'),
         ('NOT_SAY', 'Prefer Not To Say'),
     )
-    
+
     CIV_STAT = (
         ('SINGLE', 'Single'),
         ('MARRIED', 'Married'),
@@ -53,37 +53,37 @@ class Client(models.Model):
     # contact_number = models.PhoneNumberField(null-False, blank=False, unique=False)
     email = models.EmailField(blank=True)
     role=models.ForeignKey(Role, on_delete=models.PROTECT)
-    
+
     def __str__(self):
         return '%s, %s %s' % (self.last_name, self.first_name, self.middle_name)
-    
-    
+
+
     def get_payments(self):
         return self.paymentdetails.select_related('payor')
-    
-    
-    
+
+
+
 class Designation(models.Model):
     designation_name = models.CharField(max_length=32)
     description = models.CharField(max_length=32)
-    
+
     def __str__(self):
         return self.designation_name
-    
-    
-    
+
+
+
 class Agent(models.Model):
     # first_name = models.CharField(max_length=32)
     # middle_name = models.CharField(max_length=32)
     # last_name = models.CharField(max_length=32)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null = True)
-    branch = models.ForeignKey(Branch, on_delete=models.PROTECT)
-    designation = models.ForeignKey(Designation, on_delete=models.PROTECT)
-    
+    branch = models.ForeignKey(Branch, on_delete=models.PROTECT, null = True)
+    designation = models.ForeignKey(Designation, on_delete=models.PROTECT, null = True)
+
     def __str__(self):
         return str(self.user)
-        
-        
+
+
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
@@ -92,53 +92,53 @@ class Agent(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.agent.save()
-    
-    
-    
+
+
+
 class Credits(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=3)
     owner = models.ForeignKey(Agent, on_delete=models.PROTECT)
-    
-    
-    
+
+
+
 class PremiumAmount(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=3)
-    
-    
+
+
     def __str__(self):
         return str(self.amount)
-    
-    
-    
+
+
+
 class CutoffPeriod(models.Model):
     from_date = models.DateField()
     to_date = models.DateField()
-    
-    
+
+
     def __str__(self):
         return '%s to %s' % (self.from_date, self.to_date)
-    
-    
-    
+
+
+
 class ClientTagging(models.Model):
     tag_name = models.CharField(max_length=32)
     tag_description = models.CharField(max_length=32)
-    
-    
+
+
     def __str__(self):
         return self.tag_name
-    
-    
-    
+
+
+
 class PaymentType(models.Model):
     type_name = models.CharField(max_length=32)
-    
-    
+
+
     def __str__(self):
         return self.type_name
-    
-    
-    
+
+
+
 class PaymentDetails(models.Model):
     payor = models.ForeignKey(Client, on_delete=models.PROTECT, related_name='payor')
     encoder_branch = models.ForeignKey(Branch, related_name='encoder', on_delete=models.PROTECT)
@@ -151,8 +151,7 @@ class PaymentDetails(models.Model):
     tagging = models.ForeignKey(ClientTagging, on_delete=models.PROTECT)
     beneficiary1 = models.ForeignKey(Client, on_delete=models.PROTECT, related_name='beneficiary1')
     beneficiary2 = models.ForeignKey(Client, on_delete=models.PROTECT, related_name='beneficiary2')
-    
-    
+
+
     def __str__(self):
         return '%s %s' % (self.date_of_payment, self.payor)
-    
