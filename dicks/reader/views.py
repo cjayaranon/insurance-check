d5d80c83a63400b857fc67131f8b53d274d512e1
Cyril John <cjayaranon@gmail.com>
2021-11-02 17:42:18 +0800

    - Reports Printing    - added bootstrap classes to objects not meant for printing (e.g. page header, buttons)    - created printing.css to handle styling of output document    - date range from the form is asked from user input    - IDs of PamentTagging date range is queried then passed from FormPreview to TemplateView; sessions was utilized    - in TemplateView, display oy query items is started at PaymentTagging then FK is followed to retrieve more data    - session cannot carry datetime objects, IDs are used to pass data    - cache was not utilized because at the time of implementation a larger query might occur.

--- dicks/reader/views.py
+++ dicks/reader/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
-from enroller.models import PaymentTagging
+from enroller.models import PaymentTagging, PaymentDetails
 from formtools.preview import FormPreview
 # Create your views here.
 
@@ -58,13 +58,32 @@ class OwnBranchSalesFormPreview(FormPreview):
     '''
     
     def done(self, request, cleaned_data):
-        # data = **cleaned_data
         print(cleaned_data)
-        
+        # GET records from db (go thru PaymentTagging to access PaymentDetails)
+        query_list = PaymentTagging.objects.filter(payment__date_of_payment__range = [cleaned_data['from_date'], cleaned_data['to_date']]).values_list('id', flat = True)
+        # query_list = PaymentDetails.objects.filter(date_of_payment__range = [cleaned_data['from_date'], cleaned_data['to_date']])
+        # pass results to view
+        date_range = list((str(cleaned_data['from_date']), str(cleaned_data['to_date'])))
+        request.session['generated_report'] = list(query_list)
+        request.session['date_range'] = date_range
         # change to results viewing
-        return HttpResponseRedirect(reverse('pay-approver-home'))
+        return HttpResponseRedirect(reverse('reports-viewing'))
         
         
         
 class OwnBranchSalesResultsView(generic.TemplateView):
     template_name = 'read/sales-view.html'
+    
+    
+    def get(self, request, *args, **kwargs):
+        query_list = request.session.get('generated_report')
+        result_list = []
+        for items in query_list:
+            result_list.append(PaymentTagging.objects.get(id=items))
+        browser_details = list((
+            request.META['REMOTE_ADDR'],
+            request.META['HTTP_HOST'],
+            request.META['HTTP_SEC_CH_UA'],
+            request.META['OS']))
+        print(request.session.get('date_range'))
+        return render(request, self.template_name, {'result_list':result_list, 'browser_details':browser_details})
